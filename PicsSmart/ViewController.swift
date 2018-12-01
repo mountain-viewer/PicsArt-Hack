9ff4e86d38e1bd4f83db0b731104d1952f45c4f9
Yaroslav <yarspirin@yandex-team.ru>
2018-12-01 23:32:28 +0300

    Initial Commit

---
+++ PicsSmart/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PicsSmart
+//
+//  Created by Iaroslav Spirin on 12/1/18.
+//  Copyright © 2018 Mountain Viewer. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
